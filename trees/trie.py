b4dfe5395a51bf2eb423519ceb5a9f0888d90f72
devchoplife <dritedurojaiye@gmail.com>
2022-03-28 21:44:09 +0100

    Trie Implementation

---
+++ trees/trie.py
@@ -0,0 +1,35 @@
+class trieNode():
+    def __init__(self):
+        self.children = [None] * 26
+        self.isEnd = False
+
+
+class Trie():
+    def __init__(self):
+        self.root = trieNode()
+
+    def characterIndex(self, char):
+        if char.isupper():
+            return ord(char) - ord('A')
+        else:
+            return ord(char) - ('a')
+
+    # Time complexity for the insert method is O(n)
+    def insert(self, string):
+        pointer = self.root
+        for character in string:
+            index = self.characterIndex(character)
+            if not pointer.children[index]:
+                pointer.children[index] = trieNode()
+            pointer = pointer.children[index]
+        pointer.isEnd = True
+        return
+
+    def search(self, string):
+        pointer = self.root
+        for character in string:
+            index = self.characterIndex(string)
+            if not pointer.children[index]:
+                return False
+            pointer = pointer.children[index]
+        return pointer and pointer.isEnd
